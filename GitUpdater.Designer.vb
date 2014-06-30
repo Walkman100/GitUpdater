dceca537e03f2eb089fc43c1d5b82878576cd9c7
░▒▓█│Walkman│█▓▒░ <matthewcarter17@gmail.com>
2014-07-01 01:36:42 +0200

    GUI done, populate code should be done

---
+++ GitUpdater.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GitUpdater
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnGitPullAll = New System.Windows.Forms.Button()
+        Me.btnGitPushAll = New System.Windows.Forms.Button()
+        Me.lstDirs = New System.Windows.Forms.ListBox()
+        Me.btnPopulate = New System.Windows.Forms.Button()
+        Me.btnGitPullSelected = New System.Windows.Forms.Button()
+        Me.btnGitPushSelected = New System.Windows.Forms.Button()
+        Me.btnCD = New System.Windows.Forms.Button()
+        Me.btnGitPushNotSelected = New System.Windows.Forms.Button()
+        Me.btnGitPullNotSelected = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(392, 128)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(120, 23)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "Close"
+        Me.btnExit.UseVisualStyleBackColor = true
+        AddHandler Me.btnExit.Click, AddressOf Me.btnExit_Click
+        '
+        'btnGitPullAll
+        '
+        Me.btnGitPullAll.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnGitPullAll.Location = New System.Drawing.Point(266, 12)
+        Me.btnGitPullAll.Name = "btnGitPullAll"
+        Me.btnGitPullAll.Size = New System.Drawing.Size(120, 23)
+        Me.btnGitPullAll.TabIndex = 11
+        Me.btnGitPullAll.Text = "Git Pull all"
+        Me.btnGitPullAll.UseVisualStyleBackColor = true
+        '
+        'btnGitPushAll
+        '
+        Me.btnGitPushAll.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnGitPushAll.Location = New System.Drawing.Point(392, 12)
+        Me.btnGitPushAll.Name = "btnGitPushAll"
+        Me.btnGitPushAll.Size = New System.Drawing.Size(120, 23)
+        Me.btnGitPushAll.TabIndex = 12
+        Me.btnGitPushAll.Text = "Git Push all"
+        Me.btnGitPushAll.UseVisualStyleBackColor = true
+        '
+        'lstDirs
+        '
+        Me.lstDirs.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+                        Or System.Windows.Forms.AnchorStyles.Left)  _
+                        Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.lstDirs.FormattingEnabled = true
+        Me.lstDirs.HorizontalScrollbar = true
+        Me.lstDirs.IntegralHeight = false
+        Me.lstDirs.Location = New System.Drawing.Point(12, 12)
+        Me.lstDirs.Name = "lstDirs"
+        Me.lstDirs.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.lstDirs.Size = New System.Drawing.Size(248, 137)
+        Me.lstDirs.Sorted = true
+        Me.lstDirs.TabIndex = 13
+        '
+        'btnPopulate
+        '
+        Me.btnPopulate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnPopulate.Location = New System.Drawing.Point(266, 99)
+        Me.btnPopulate.Name = "btnPopulate"
+        Me.btnPopulate.Size = New System.Drawing.Size(120, 23)
+        Me.btnPopulate.TabIndex = 14
+        Me.btnPopulate.Text = "Populate"
+        Me.btnPopulate.UseVisualStyleBackColor = true
+        AddHandler Me.btnPopulate.Click, AddressOf Me.BtnPopulate_Click
+        '
+        'btnGitPullSelected
+        '
+        Me.btnGitPullSelected.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnGitPullSelected.Location = New System.Drawing.Point(266, 41)
+        Me.btnGitPullSelected.Name = "btnGitPullSelected"
+        Me.btnGitPullSelected.Size = New System.Drawing.Size(120, 23)
+        Me.btnGitPullSelected.TabIndex = 15
+        Me.btnGitPullSelected.Text = "Git Pull selected"
+        Me.btnGitPullSelected.UseVisualStyleBackColor = true
+        '
+        'btnGitPushSelected
+        '
+        Me.btnGitPushSelected.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnGitPushSelected.Location = New System.Drawing.Point(392, 41)
+        Me.btnGitPushSelected.Name = "btnGitPushSelected"
+        Me.btnGitPushSelected.Size = New System.Drawing.Size(120, 23)
+        Me.btnGitPushSelected.TabIndex = 16
+        Me.btnGitPushSelected.Text = "Git Push selected"
+        Me.btnGitPushSelected.UseVisualStyleBackColor = true
+        '
+        'btnCD
+        '
+        Me.btnCD.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnCD.Location = New System.Drawing.Point(392, 99)
+        Me.btnCD.Name = "btnCD"
+        Me.btnCD.Size = New System.Drawing.Size(120, 23)
+        Me.btnCD.TabIndex = 17
+        Me.btnCD.Text = "Change Directory..."
+        Me.btnCD.UseVisualStyleBackColor = true
+        '
+        'btnGitPushNotSelected
+        '
+        Me.btnGitPushNotSelected.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnGitPushNotSelected.Location = New System.Drawing.Point(392, 70)
+        Me.btnGitPushNotSelected.Name = "btnGitPushNotSelected"
+        Me.btnGitPushNotSelected.Size = New System.Drawing.Size(120, 23)
+        Me.btnGitPushNotSelected.TabIndex = 18
+        Me.btnGitPushNotSelected.Text = "...all except selected"
+        Me.btnGitPushNotSelected.UseVisualStyleBackColor = true
+        '
+        'btnGitPullNotSelected
+        '
+        Me.btnGitPullNotSelected.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnGitPullNotSelected.Location = New System.Drawing.Point(266, 70)
+        Me.btnGitPullNotSelected.Name = "btnGitPullNotSelected"
+        Me.btnGitPullNotSelected.Size = New System.Drawing.Size(120, 23)
+        Me.btnGitPullNotSelected.TabIndex = 19
+        Me.btnGitPullNotSelected.Text = "...all except selected"
+        Me.btnGitPullNotSelected.UseVisualStyleBackColor = true
+        '
+        'GitUpdater
+        '
+        Me.AcceptButton = Me.btnPopulate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(524, 163)
+        Me.Controls.Add(Me.btnGitPullNotSelected)
+        Me.Controls.Add(Me.btnGitPushNotSelected)
+        Me.Controls.Add(Me.btnCD)
+        Me.Controls.Add(Me.btnGitPushSelected)
+        Me.Controls.Add(Me.btnGitPullSelected)
+        Me.Controls.Add(Me.btnPopulate)
+        Me.Controls.Add(Me.lstDirs)
+        Me.Controls.Add(Me.btnGitPushAll)
+        Me.Controls.Add(Me.btnGitPullAll)
+        Me.Controls.Add(Me.btnExit)
+        Me.HelpButton = true
+        Me.Name = "GitUpdater"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GitUpdater"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(224,Byte),Integer), CType(CType(224,Byte),Integer), CType(CType(224,Byte),Integer))
+        Me.ResumeLayout(false)
+    End Sub
+    Private btnGitPullNotSelected As System.Windows.Forms.Button
+    Private btnGitPushNotSelected As System.Windows.Forms.Button
+    Private btnCD As System.Windows.Forms.Button
+    Private btnGitPushSelected As System.Windows.Forms.Button
+    Private btnGitPullSelected As System.Windows.Forms.Button
+    Private btnPopulate As System.Windows.Forms.Button
+    Private lstDirs As System.Windows.Forms.ListBox
+    Private btnGitPushAll As System.Windows.Forms.Button
+    Private btnGitPullAll As System.Windows.Forms.Button
+    Private btnExit As System.Windows.Forms.Button
+End Class
---
+++ GitUpdater.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.IO
+
+Public Class GitUpdater
+    
+    Dim usrProfile As String = Environment.GetEnvironmentVariable("HOMEPATH")
+    Dim Dir As String = usrProfile & "\Documents\GitHub"
+    
+    Private Sub btnExit_Click(sender As Object, e As EventArgs)
+        End
+    End Sub
+    
+    Sub BtnPopulate_Click(sender As Object, e As EventArgs)
+        For Each Repo As String In Directory.GetDirectories(Dir)
+            lstDirs.Items.Add(Mid(Repo, Len(Dir) + 2))
+        Next
+    End Sub
+End Class
