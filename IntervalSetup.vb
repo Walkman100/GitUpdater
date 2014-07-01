fc1633da42ccb7b1d8963fccf25973b39e723741
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-07-01 20:18:18 +0200

    Adding my files.

---
+++ IntervalSetup.vb
@@ -0,0 +1,22 @@
+﻿'
+' Created by SharpDevelop.
+' User: Tristan
+' Date: 7/1/2014
+' Time: 8:02 PM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Public Partial Class Interval
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+	
+	Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles textBox1.TextChanged 
+		
+	End Sub
+End Class
