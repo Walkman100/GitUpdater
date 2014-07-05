7c234dc90172197bd6c0253993d9d4724a9b559e
░▒▓█│Walkman│█▓▒░ <matthewcarter17@gmail.com>
2014-07-06 01:02:10 +0200

    Removed progress bar and credentials management, fixed up GUI

--- GitUpdater.Designer.vb
+++ GitUpdater.Designer.vb
@@ -22,7 +22,6 @@ Partial Class GitUpdater
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(GitUpdater))
         Me.btnExit = New System.Windows.Forms.Button()
         Me.btnGitPullAll = New System.Windows.Forms.Button()
@@ -39,28 +38,20 @@ Partial Class GitUpdater
         Me.chkPushForce = New System.Windows.Forms.CheckBox()
         Me.chkDontClose = New System.Windows.Forms.CheckBox()
         Me.chkNoWait = New System.Windows.Forms.CheckBox()
-        Me.progressBar = New System.Windows.Forms.ProgressBar()
-        Me.grpOptions = New System.Windows.Forms.GroupBox()
-        Me.grpCreds = New System.Windows.Forms.GroupBox()
-        Me.btnShowPass = New System.Windows.Forms.Button()
-        Me.lblSaved = New System.Windows.Forms.Label()
-        Me.btnSave = New System.Windows.Forms.Button()
-        Me.txtPassword = New System.Windows.Forms.TextBox()
-        Me.txtUsername = New System.Windows.Forms.TextBox()
-        Me.lblPassword = New System.Windows.Forms.Label()
-        Me.lblUsername = New System.Windows.Forms.Label()
-        Me.timerKeyChecker = New System.Windows.Forms.Timer(Me.components)
-        Me.grpOptions.SuspendLayout
-        Me.grpCreds.SuspendLayout
+        Me.grpGUI = New System.Windows.Forms.GroupBox()
+        Me.chkDontShow = New System.Windows.Forms.CheckBox()
+        Me.grpData = New System.Windows.Forms.GroupBox()
+        Me.grpGUI.SuspendLayout
+        Me.grpData.SuspendLayout
         Me.SuspendLayout
         '
         'btnExit
         '
         Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnExit.Location = New System.Drawing.Point(392, 328)
+        Me.btnExit.Location = New System.Drawing.Point(434, 363)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(120, 23)
+        Me.btnExit.Size = New System.Drawing.Size(78, 23)
         Me.btnExit.TabIndex = 10
         Me.btnExit.Text = "Close"
         Me.btnExit.UseVisualStyleBackColor = true
@@ -99,16 +90,16 @@ Partial Class GitUpdater
         Me.lstDirs.Location = New System.Drawing.Point(12, 12)
         Me.lstDirs.Name = "lstDirs"
         Me.lstDirs.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
-        Me.lstDirs.Size = New System.Drawing.Size(248, 339)
+        Me.lstDirs.Size = New System.Drawing.Size(248, 374)
         Me.lstDirs.Sorted = true
         Me.lstDirs.TabIndex = 13
         '
         'btnRefresh
         '
         Me.btnRefresh.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.btnRefresh.Location = New System.Drawing.Point(266, 299)
+        Me.btnRefresh.Location = New System.Drawing.Point(266, 363)
         Me.btnRefresh.Name = "btnRefresh"
-        Me.btnRefresh.Size = New System.Drawing.Size(120, 23)
+        Me.btnRefresh.Size = New System.Drawing.Size(78, 23)
         Me.btnRefresh.TabIndex = 14
         Me.btnRefresh.Text = "Refresh"
         Me.btnRefresh.UseVisualStyleBackColor = true
@@ -139,9 +130,9 @@ Partial Class GitUpdater
         'btnCD
         '
         Me.btnCD.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.btnCD.Location = New System.Drawing.Point(392, 299)
+        Me.btnCD.Location = New System.Drawing.Point(350, 363)
         Me.btnCD.Name = "btnCD"
-        Me.btnCD.Size = New System.Drawing.Size(120, 23)
+        Me.btnCD.Size = New System.Drawing.Size(78, 23)
         Me.btnCD.TabIndex = 17
         Me.btnCD.Text = "Change Directory..."
         Me.btnCD.UseVisualStyleBackColor = true
@@ -171,12 +162,11 @@ Partial Class GitUpdater
         '
         'chkRepeat
         '
-        Me.chkRepeat.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.chkRepeat.AutoSize = true
         Me.chkRepeat.BackColor = System.Drawing.Color.Transparent
         Me.chkRepeat.Checked = true
         Me.chkRepeat.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.chkRepeat.Location = New System.Drawing.Point(272, 338)
+        Me.chkRepeat.Location = New System.Drawing.Point(6, 42)
         Me.chkRepeat.Name = "chkRepeat"
         Me.chkRepeat.Size = New System.Drawing.Size(125, 17)
         Me.chkRepeat.TabIndex = 20
@@ -190,10 +180,9 @@ Partial Class GitUpdater
         '
         'chkPushForce
         '
-        Me.chkPushForce.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
         Me.chkPushForce.AutoSize = true
         Me.chkPushForce.BackColor = System.Drawing.Color.Transparent
-        Me.chkPushForce.Location = New System.Drawing.Point(272, 321)
+        Me.chkPushForce.Location = New System.Drawing.Point(6, 19)
         Me.chkPushForce.Name = "chkPushForce"
         Me.chkPushForce.Size = New System.Drawing.Size(116, 17)
         Me.chkPushForce.TabIndex = 21
@@ -221,124 +210,41 @@ Partial Class GitUpdater
         Me.chkNoWait.Text = "Don't wait for cmd to close before starting next"
         Me.chkNoWait.UseVisualStyleBackColor = false
         '
-        'progressBar
+        'grpGUI
         '
-        Me.progressBar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.progressBar.Location = New System.Drawing.Point(266, 99)
-        Me.progressBar.Name = "progressBar"
-        Me.progressBar.Size = New System.Drawing.Size(246, 18)
-        Me.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous
-        Me.progressBar.TabIndex = 24
-        Me.progressBar.Visible = false
-        '
-        'grpOptions
-        '
-        Me.grpOptions.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.grpOptions.Controls.Add(Me.chkNoWait)
-        Me.grpOptions.Controls.Add(Me.chkDontClose)
-        Me.grpOptions.Location = New System.Drawing.Point(266, 229)
-        Me.grpOptions.Name = "grpOptions"
-        Me.grpOptions.Size = New System.Drawing.Size(246, 64)
-        Me.grpOptions.TabIndex = 25
-        Me.grpOptions.TabStop = false
-        Me.grpOptions.Text = "Options"
-        '
-        'grpCreds
-        '
-        Me.grpCreds.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+        Me.grpGUI.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
                         Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.grpCreds.Controls.Add(Me.btnShowPass)
-        Me.grpCreds.Controls.Add(Me.lblSaved)
-        Me.grpCreds.Controls.Add(Me.btnSave)
-        Me.grpCreds.Controls.Add(Me.txtPassword)
-        Me.grpCreds.Controls.Add(Me.txtUsername)
-        Me.grpCreds.Controls.Add(Me.lblPassword)
-        Me.grpCreds.Controls.Add(Me.lblUsername)
-        Me.grpCreds.Location = New System.Drawing.Point(266, 123)
-        Me.grpCreds.Name = "grpCreds"
-        Me.grpCreds.Size = New System.Drawing.Size(246, 100)
-        Me.grpCreds.TabIndex = 26
-        Me.grpCreds.TabStop = false
-        Me.grpCreds.Text = "Credientials Management"
+        Me.grpGUI.Controls.Add(Me.chkDontShow)
+        Me.grpGUI.Controls.Add(Me.chkNoWait)
+        Me.grpGUI.Controls.Add(Me.chkDontClose)
+        Me.grpGUI.Location = New System.Drawing.Point(266, 99)
+        Me.grpGUI.Name = "grpGUI"
+        Me.grpGUI.Size = New System.Drawing.Size(246, 187)
+        Me.grpGUI.TabIndex = 25
+        Me.grpGUI.TabStop = false
+        Me.grpGUI.Text = "GUI Options"
         '
-        'btnShowPass
+        'chkDontShow
         '
-        Me.btnShowPass.BackColor = System.Drawing.Color.White
-        Me.btnShowPass.FlatAppearance.BorderColor = System.Drawing.Color.White
-        Me.btnShowPass.FlatAppearance.BorderSize = 0
-        Me.btnShowPass.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlLight
-        Me.btnShowPass.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control
-        Me.btnShowPass.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnShowPass.ForeColor = System.Drawing.Color.Transparent
-        Me.btnShowPass.Image = CType(resources.GetObject("btnShowPass.Image"),System.Drawing.Image)
-        Me.btnShowPass.Location = New System.Drawing.Point(218, 47)
-        Me.btnShowPass.Name = "btnShowPass"
-        Me.btnShowPass.Size = New System.Drawing.Size(20, 16)
-        Me.btnShowPass.TabIndex = 27
-        Me.btnShowPass.UseVisualStyleBackColor = false
-        AddHandler Me.btnShowPass.MouseDown, AddressOf Me.btnShowPass_MouseDown
-        AddHandler Me.btnShowPass.MouseUp, AddressOf Me.btnShowPass_MouseUp
+        Me.chkDontShow.AutoSize = true
+        Me.chkDontShow.Location = New System.Drawing.Point(6, 65)
+        Me.chkDontShow.Name = "chkDontShow"
+        Me.chkDontShow.Size = New System.Drawing.Size(178, 17)
+        Me.chkDontShow.TabIndex = 24
+        Me.chkDontShow.Text = "Don't show command window(s)"
+        Me.chkDontShow.UseVisualStyleBackColor = true
         '
-        'lblSaved
+        'grpData
         '
-        Me.lblSaved.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.lblSaved.AutoSize = true
-        Me.lblSaved.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
-        Me.lblSaved.ForeColor = System.Drawing.Color.Green
-        Me.lblSaved.Location = New System.Drawing.Point(112, 76)
-        Me.lblSaved.Name = "lblSaved"
-        Me.lblSaved.Size = New System.Drawing.Size(47, 13)
-        Me.lblSaved.TabIndex = 5
-        Me.lblSaved.Text = "Saved!"
-        Me.lblSaved.Visible = false
-        '
-        'btnSave
-        '
-        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
-        Me.btnSave.Location = New System.Drawing.Point(165, 71)
-        Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(75, 23)
-        Me.btnSave.TabIndex = 4
-        Me.btnSave.Text = "Save"
-        Me.btnSave.UseVisualStyleBackColor = true
-        AddHandler Me.btnSave.Click, AddressOf Me.BtnSave_Click
-        '
-        'txtPassword
-        '
-        Me.txtPassword.Location = New System.Drawing.Point(70, 45)
-        Me.txtPassword.Name = "txtPassword"
-        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9679)
-        Me.txtPassword.Size = New System.Drawing.Size(170, 20)
-        Me.txtPassword.TabIndex = 3
-        '
-        'txtUsername
-        '
-        Me.txtUsername.Location = New System.Drawing.Point(70, 19)
-        Me.txtUsername.Name = "txtUsername"
-        Me.txtUsername.Size = New System.Drawing.Size(170, 20)
-        Me.txtUsername.TabIndex = 2
-        '
-        'lblPassword
-        '
-        Me.lblPassword.AutoSize = true
-        Me.lblPassword.Location = New System.Drawing.Point(6, 48)
-        Me.lblPassword.Name = "lblPassword"
-        Me.lblPassword.Size = New System.Drawing.Size(56, 13)
-        Me.lblPassword.TabIndex = 1
-        Me.lblPassword.Text = "Password:"
-        '
-        'lblUsername
-        '
-        Me.lblUsername.AutoSize = true
-        Me.lblUsername.Location = New System.Drawing.Point(6, 22)
-        Me.lblUsername.Name = "lblUsername"
-        Me.lblUsername.Size = New System.Drawing.Size(58, 13)
-        Me.lblUsername.TabIndex = 0
-        Me.lblUsername.Text = "Username:"
-        '
-        'timerKeyChecker
-        '
-        AddHandler Me.timerKeyChecker.Tick, AddressOf Me.TimerKeyChecker_Tick
+        Me.grpData.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.grpData.Controls.Add(Me.chkPushForce)
+        Me.grpData.Controls.Add(Me.chkRepeat)
+        Me.grpData.Location = New System.Drawing.Point(266, 292)
+        Me.grpData.Name = "grpData"
+        Me.grpData.Size = New System.Drawing.Size(246, 65)
+        Me.grpData.TabIndex = 26
+        Me.grpData.TabStop = false
+        Me.grpData.Text = "Data Options"
         '
         'GitUpdater
         '
@@ -346,14 +252,12 @@ Partial Class GitUpdater
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.btnExit
-        Me.ClientSize = New System.Drawing.Size(524, 363)
+        Me.ClientSize = New System.Drawing.Size(524, 398)
+        Me.Controls.Add(Me.grpData)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnCD)
         Me.Controls.Add(Me.btnRefresh)
-        Me.Controls.Add(Me.grpCreds)
-        Me.Controls.Add(Me.grpOptions)
-        Me.Controls.Add(Me.progressBar)
-        Me.Controls.Add(Me.chkPushForce)
+        Me.Controls.Add(Me.grpGUI)
         Me.Controls.Add(Me.btnGitPullNotSelected)
         Me.Controls.Add(Me.btnGitPushNotSelected)
         Me.Controls.Add(Me.btnGitPushSelected)
@@ -361,7 +265,6 @@ Partial Class GitUpdater
         Me.Controls.Add(Me.lstDirs)
         Me.Controls.Add(Me.btnGitPushAll)
         Me.Controls.Add(Me.btnGitPullAll)
-        Me.Controls.Add(Me.chkRepeat)
         Me.HelpButton = true
         Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
         Me.Name = "GitUpdater"
@@ -370,24 +273,15 @@ Partial Class GitUpdater
         Me.Text = "GitUpdater"
         Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(224,Byte),Integer), CType(CType(224,Byte),Integer), CType(CType(224,Byte),Integer))
         AddHandler Load, AddressOf Me.GitUpdater_Load
-        Me.grpOptions.ResumeLayout(false)
-        Me.grpOptions.PerformLayout
-        Me.grpCreds.ResumeLayout(false)
-        Me.grpCreds.PerformLayout
+        Me.grpGUI.ResumeLayout(false)
+        Me.grpGUI.PerformLayout
+        Me.grpData.ResumeLayout(false)
+        Me.grpData.PerformLayout
         Me.ResumeLayout(false)
-        Me.PerformLayout
     End Sub
-    Private btnShowPass As System.Windows.Forms.Button
-    Private timerKeyChecker As System.Windows.Forms.Timer
-    Private lblUsername As System.Windows.Forms.Label
-    Private lblPassword As System.Windows.Forms.Label
-    Private txtUsername As System.Windows.Forms.TextBox
-    Private txtPassword As System.Windows.Forms.TextBox
-    Private btnSave As System.Windows.Forms.Button
-    Private lblSaved As System.Windows.Forms.Label
-    Private grpCreds As System.Windows.Forms.GroupBox
-    Private grpOptions As System.Windows.Forms.GroupBox
-    Private progressBar As System.Windows.Forms.ProgressBar
+    Private grpGUI As System.Windows.Forms.GroupBox
+    Private grpData As System.Windows.Forms.GroupBox
+    Private chkDontShow As System.Windows.Forms.CheckBox
     Private chkNoWait As System.Windows.Forms.CheckBox
     Private chkDontClose As System.Windows.Forms.CheckBox
     Private chkPushForce As System.Windows.Forms.CheckBox
--- GitUpdater.vb
+++ GitUpdater.vb
@@ -11,36 +11,11 @@ Public Class GitUpdater
         End
     End Sub
     
-    Sub BtnSave_Click(sender As Object, e As EventArgs)
-        My.Settings.Username = txtUsername.Text
-        My.Settings.Password = txtPassword.Text
-        lblSaved.Visible = True
-        ' wait 2 seconds
-        lblSaved.Visible = False
-    End Sub
-    
-    Sub btnShowPass_MouseDown(sender As Object, e As EventArgs)
-        txtPassword.PasswordChar = ""
-    End Sub
-    
-    Sub btnShowPass_MouseUp(sender As Object, e As EventArgs)
-        txtPassword.PasswordChar = "●"
-    End Sub
-    
-    Sub TimerKeyChecker_Tick(sender As Object, e As EventArgs)
-        If My.Computer.Keyboard.CtrlKeyDown = True And My.Computer.Keyboard.ShiftKeyDown = True Then
-            SendKeys.send(txtUsername.Text & "{ENTER}")
-            SendKeys.send(txtPassword.Text & "{ENTER}")
-        End If
-    End Sub
-    
     Sub GitUpdater_Load(sender As Object, e As EventArgs)
         lstDirs.Items.Clear
         For Each Repo As String In Directory.GetDirectories(Dir)
             lstDirs.Items.Add(Mid(Repo, Len(Dir) + 2))
         Next
-        txtUsername.Text = My.Settings.Username
-        txtPassword.Text = My.Settings.Password
     End Sub
     
     Sub BtnRefresh_Click(sender As Object, e As EventArgs)
@@ -63,20 +38,15 @@ Public Class GitUpdater
     End Sub
     
     Sub BtnGitPullAll_Click(sender As Object, e As EventArgs)
-        progressBar.Visible = True
-        progressBar.Maximum = lstDirs.Items.Count
         If chkNoWait.Checked = False Then
             For i = 1 To lstDirs.Items.Count
-                progressBar.Value = i
                 Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " pull " & chkRepeat.Checked & " " & chkDontClose.Checked, vbNormalFocus, True)
             Next
         Else
             For i = 1 To lstDirs.Items.Count
-                progressBar.Value = i
                 Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " pull " & chkRepeat.Checked & " " & chkDontClose.Checked, vbNormalFocus, True, 1000)
             Next
         End If
-        progressBar.Visible = False
         
 ' another way to do a comment block: http://forums.asp.net/post/4414215.aspx
 '            'MsgBox("/k cd " & Dir & "\" & lstDirs.Items.Item(i - 1))
@@ -95,97 +65,72 @@ Public Class GitUpdater
     End Sub
     
     Sub BtnGitPushAll_Click(sender As Object, e As EventArgs)
-        timerKeyChecker.Start
-        progressBar.Visible = True
-        progressBar.Maximum = lstDirs.Items.Count
         If chkPushForce.Checked = True Then ForcePush = "-f"
         If chkNoWait.Checked = False Then
             For i = 1 To lstDirs.Items.Count
-                progressBar.Value = i
                 Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " push " & chkRepeat.Checked & " " & chkDontClose.Checked & " " & ForcePush, vbNormalFocus, True)
             Next
         Else
             For i = 1 To lstDirs.Items.Count
-                progressBar.Value = i
                 Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " push " & chkRepeat.Checked & " " & chkDontClose.Checked & " " & ForcePush, vbNormalFocus, True, 1000)
             Next
         End If
-        progressBar.Visible = False
-        timerKeyChecker.Stop
     End Sub
     
     Sub BtnGitPullSelected_Click(sender As Object, e As EventArgs)
-        timerKeyChecker.Start
         If lstDirs.SelectedIndex = -1 Then
             MsgBox("No item selected")
         Else
             Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(lstDirs.SelectedIndex) & " pull " & chkRepeat.Checked & " " & chkDontClose.Checked, vbNormalFocus, True, 1000)
         End If
-        timerKeyChecker.Stop
     End Sub
     
     Sub BtnGitPushSelected_Click(sender As Object, e As EventArgs)
-        timerKeyChecker.Start
         If chkPushForce.Checked = True Then ForcePush = "-f"
         If lstDirs.SelectedIndex = -1 Then
             MsgBox("No item selected")
         Else
             Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(lstDirs.SelectedIndex) & " push " & chkRepeat.Checked & " " & chkDontClose.Checked & " " & ForcePush, vbNormalFocus, True, 1000)
         End If
-        timerKeyChecker.Stop
     End Sub
     
     Sub BtnGitPullNotSelected_Click(sender As Object, e As EventArgs)
-        timerKeyChecker.Start
         If lstDirs.SelectedIndex = -1 Then
             MsgBox("No item selected")
         Else
-            progressBar.Visible = True
-            progressBar.Maximum = lstDirs.Items.Count
             If chkNoWait.Checked = False Then
                 For i = 1 To lstDirs.Items.Count
                     If i - 1 <> lstDirs.SelectedIndex Then
-                        progressBar.Value = i
                         Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " pull " & chkRepeat.Checked & " " & chkDontClose.Checked, vbNormalFocus, True)
                     End If
                 Next
             Else
                 For i = 1 To lstDirs.Items.Count
                     If i - 1 <> lstDirs.SelectedIndex Then
-                        progressBar.Value = i
                         Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " pull " & chkRepeat.Checked & " " & chkDontClose.Checked, vbNormalFocus, True, 1000)
                     End If
                 Next
             End If
-            progressBar.Visible = False
         End If
-        timerKeyChecker.Stop
     End Sub
     
     Sub BtnGitPushNotSelected_Click(sender As Object, e As EventArgs)
-        timerKeyChecker.Start
         If lstDirs.SelectedIndex = -1 Then
             MsgBox("No item selected")
         Else
-            progressBar.Visible = True
-            progressBar.Maximum = lstDirs.Items.Count
             If chkNoWait.Checked = False Then
                 For i = 1 To lstDirs.Items.Count
                     If i - 1 <> lstDirs.SelectedIndex Then
-                        progressBar.Value = i
                         Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " pull " & chkRepeat.Checked & " " & chkDontClose.Checked, vbNormalFocus, True)
                     End If
                 Next
             Else
                 For i = 1 To lstDirs.Items.Count
                     If i - 1 <> lstDirs.SelectedIndex Then
-                        progressBar.Value = i
                         Shell("GitUpdater.bat " & Dir & "\" & lstDirs.Items.Item(i - 1) & " pull " & chkRepeat.Checked & " " & chkDontClose.Checked, vbNormalFocus, True, 1000)
                     End If
                 Next
             End If
-            progressBar.Visible = False
         End If
-        timerKeyChecker.Stop
     End Sub
 End Class
