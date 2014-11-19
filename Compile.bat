1f9ca0e897a8bc909405dfd5a4155e17adddf8a0
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-11-19 03:19:46 +0200

    Fixed a bug in Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -1,11 +1,12 @@
 @echo off
 
-%ProgramFiles%\MSBuild\12.0\bin\msbuild.exe /property:OutDir=bin\Release\ GitUpdater.sln
+%ProgramFiles%\MSBuild\12.0\bin\msbuild.exe /property:Configuration=Release GitUpdater.sln
 
 %ProgramFiles%\NSIS\makensis.exe "NSIS Installer for GitUpdater.nsi"
 
-rem use this if you want it to ask where to zip to
+rem use this if you want it to ask where to rar to
 rem %ProgramFiles%\WinRAR\WinRAR.exe a -ep1 -scul -r0 -iext --. %~dp0bin\Release\GitUpdater.exe %~dp0bin\Release\GitUpdater.bat %~dp0bin\Release\OpenRepoInPS.bat %~dp0bin\Release\PS
+
 %ProgramFiles%\WinRAR\WinRAR.exe a -ep1 -scul -r0 -iext -- %~dp0bin\Release\GitUpdater-Portable.rar %~dp0bin\Release\GitUpdater.exe %~dp0bin\Release\GitUpdater.bat %~dp0bin\Release\OpenRepoInPS.bat %~dp0bin\Release\PS
 
 explorer.exe "%~dp0bin\Release"
